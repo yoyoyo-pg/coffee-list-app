f71f36650c72a7916beb76251123d1d50e15572e
yoyoyo-pg <akiyoshiki1023@gmail.com>
2024-05-06 23:29:33 +0900

    create-coffee-list-api

---
+++ src/CoffeeList.js
@@ -0,0 +1,45 @@
+import {useEffect, useState} from 'react';
+
+export default function App() {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    const resp = await fetch('https://api.sampleapis.com/coffee/hot');
+    const json = await resp.json();
+    setData(json);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <img
+                src={item.image}
+                alt={item.title}
+                style={{
+                  width: '10%',
+                  height: 'auto',
+                }}
+              />
+              <h3>Ingredients:</h3>
+              <ul>
+                {item.ingredients.map((ingredient, index) => (
+                  <li key={`${item.id}-${index}`}>{ingredient}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
